71fd3532a5ae9115574cacace828001969bd5744
philipjpark <66290921+philipjpark@users.noreply.github.com>
2022-02-27 11:13:22 -0500

    First commit

---
+++ Data Types.py
@@ -0,0 +1,41 @@
+print ("Addition: {} + {} = {}".format(10,5,10+5))
+
+print ("Subtraction: {} - {} = {}".format(10,5,10-5))
+
+print ("Multiplication: {} * {} = {}".format(10,5,10*5))
+
+print ("Division: {} / {} = {}".format(10.0,4.0,10.0/4.0))
+
+print ("Exponentitation: {} ** {} = {}".format(10,4,10**4))
+
+print ("Modulo: {} % {} = {}\n".format(9,4,10%4))
+
+print ("Greater than: {} > {} = {}".format(10,5,10>5))
+
+print ("Less than: {} < {} = {}".format(10,5,10<5))
+
+print ("Equals: {} == {} = {}".format(10,5,10==5))
+
+print ("Doesn't Equal: {} <> {} = {}".format(10,5,10 != 5))
+print ("None's Boolean Type: (bool({}) == {}) = {}".format(None,False,bool(None)==False))
+
+# If statement example
+
+x = 10
+    
+if x <= 10 == 10:
+    print (x + 2)
+        
+n = 5
+if n > 10:
+    print (n + 2)
+    
+if n < 8:
+    print ('Philip')
+    
+elif n > 20:
+    print (n + 4)
+else:
+    print (n)
+    
+   
